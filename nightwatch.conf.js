b9417fecb944ae9068ff319bfae5a43ff647d81b
Yasar Sazak <y.sazak@nl.emakina.com>
2020-02-16 16:16:01 +0100

    friefox

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -34,7 +34,8 @@ module.exports = {
       },
 
       desiredCapabilities: {
-        browserName : 'chrome'
+        browserName : 'firefox',
+        acceptSslCerts: true,
       },
 
       webdriver: {
@@ -92,7 +93,7 @@ module.exports = {
 
       webdriver: {
         start_process: true,
-        port: 9516,
+        port: 9515,
         server_path: "./node_modules/chromedriver/lib/chroemdriver/chromedriver", // (Services.chromedriver ? Services.chromedriver.path : ''),
         cli_args: [
           // --verbose
