437a190b5f52f205fb178b2f949050aa9a46c85d
Yasar Sazak <y.sazak@nl.emakina.com>
2020-02-16 20:20:59 +0100

    path

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -41,7 +41,7 @@ module.exports = {
       webdriver: {
         start_process: true,
         port: 9515,
-        server_path: 'D:\\a\1\\s\\node_modules\\chromedriver\\lib\\chromedriver\\chromedriver.exe', //(Services.chromedriver ? Services.chromedriver.path : ''),
+        server_path: 'D:\\a\\1\\s\\node_modules\\chromedriver\\lib\\chromedriver\\chromedriver.exe', //(Services.chromedriver ? Services.chromedriver.path : ''),
         cli_args: [
           // --verbose
         ]
